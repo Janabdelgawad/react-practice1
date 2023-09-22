f5453e346153eab5200435ff44ee3ae4dd59eecf
Jana <97198479+Janabdelgawad@users.noreply.github.com>
2023-09-22 20:54:41 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favorite Food</h1>
+    <ul>
+      <li>Pasta</li>
+      <li>Pickles</li>
+      <li>Chocolate</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
